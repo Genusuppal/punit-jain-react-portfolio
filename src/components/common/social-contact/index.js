37021df7d628acd2a1c60ae157077d270478e6f4
Genusuppal <harpranav2202@gmail.com>
2023-09-16 22:58:03 +0530

    WhatsApp and Gmail Icon Added

--- src/components/common/social-contact/index.js
+++ src/components/common/social-contact/index.js
@@ -1,22 +1,22 @@
 import React from "react";
 import { SocialData } from "../../data/social";
 import "./social-contact.css";
-function SocialContact() {
+function SocialContact({ align = "center" }) {
 	const data = SocialData;
 	return (
-		<div className="social-contact">
-			<div className="resume-link">
-				<a href={require("../../../assets/RESUME.pdf")} target="_blank" rel="noreferrer">
-					Résumé
+		<div className="social-contact" style={{ "--alignment": align }}>
+			<div className="social-icon-div">
+				<a className="resume-link" href={require("../../../assets/RESUME.pdf")} target="_blank" rel="noreferrer">
+					Resume
 				</a>
 			</div>
 			{data.map((item) => {
 				return (
-					<a key={item.id} href={item.link}>
-						<div className="social-icon-div">
-							<img src={item.icon} alt="" className="social-icon" />
-						</div>
-					</a>
+					<div className="social-icon-div">
+						<a key={item.id} href={item.link}>
+							<img src={item.icon} alt="" height={32} width={32} className="social-icon" />
+						</a>
+					</div>
 				);
 			})}
 		</div>
--- src/components/data/social.js
+++ src/components/data/social.js
@@ -1,16 +1,20 @@
-export const SocialData=[
-    
-    /*{
-        id:1,
-        platform:"Github",
-        icon: require("../../assets/icons/github.png"),
-        link:"https://github.com/JokubasVaisnoras/",
-    }
-    ,*/
-    {
-        id:2,
-        platform:"Linkedin",
-        icon: require("../../assets/icons/linkedin.png"),
-        link:"https://www.linkedin.com/in/punit-jain-353197283/",
-    },
+export const SocialData = [
+	{
+		id: 1,
+		platform: "Linkedin",
+		icon: require("../../assets/icons/linkedin.png"),
+		link: "https://www.linkedin.com/in/punit-jain-353197283/",
+	},
+	{
+		id: 2,
+		platform: "Gmail",
+		icon: require("../../assets/icons/gmail.png"),
+		link: "mailto:punitja56@gmail.com",
+	},
+	{
+		id: 3,
+		platform: "WhatsApp",
+		icon: require("../../assets/icons/whatsapp.png"),
+		link: "https://api.whatsapp.com/send?phone=8278661215",
+	},
 ];
